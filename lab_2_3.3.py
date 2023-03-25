7978989ef28a35b23fd6d823173efeb30d9ced27
KS <eshchetnikova@gmail.com>
2023-03-25 22:30:23 +0700

    change

--- lab_2_3.3.py
+++ lab_2_3.3.py
@@ -1,16 +1,17 @@
 #3.3
-list = []
-list1 = []
-for _ in range(int(input())):
-  list.append(int(input()))
-print(list)
-for i in list:
+import sys
+
+args_list = sys.argv
+arr = [int(x) for x in sys.argv[1:]]
+sum_elem = 0
+for i in range (0, len(arr)):
   if i % 2 != 0:
-    list1.append(i)
-print('Сумма чисел с нечетным индексом: ', list1)
+    sum_elem = sum_elem + i
+print('Сумма нечетных чисел: ', sum_elem)
+
 m = 2
-for i in range(len(list)):
-   if list[i] < 15:
-     list[i] *= m
-     print('Удвоенное число: ', list[i])
-     print(list)
+for i in range(0, len(arr)):
+   if arr[i] < 15:
+     arr[i] *= m
+     print('Удвоенное число: ', arr[i])
+print(arr)
